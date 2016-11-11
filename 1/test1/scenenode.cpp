602b9dbeeeb4456a3646be1d39948b47e75c180e
Marat <maratf76@gmail.com>
2016-11-11 16:49:38 +0500

    4

--- 1/test1/scenenode.cpp
+++ 1/test1/scenenode.cpp
@@ -5,3 +5,20 @@ SceneNode::SceneNode()
     //ctor
     mParent = nullptr;
 }
+void SceneNode::attachChild(Ptr child)
+{
+    child->mParent = this;
+    mChildren.push_back(std::move(child));
+}
+
+SceneNode::Ptr SceneNode::detachChild(const SceneNode& node)
+{
+    auto found = std::find_if(mChildren.begin(),mChildren.end(),
+                            [&] (Ptr& p) -> bool { return p.get() == &node;});
+    assert (found != mChildren.end());
+
+    Ptr result = std::move(*found);
+    result->mParent = nullptr;
+    mChildren.erase(found);
+    return result;
+}
--- 1/test1/scenenode.h
+++ 1/test1/scenenode.h
@@ -2,15 +2,22 @@
 #define SCENENODE_H
 
 
-class SceneNode
+class SceneNode : public sf::Transformable, public sf::Drawable,
+                  private sf::NonCopyable
 {
     public:
 
         typedef std::unique_ptr<SceneNode> Ptr;
 
         SceneNode();
+        void attachChild (Ptr child);
+        Ptr detachChild(const SceneNode& node);
     protected:
     private:
+
+        virtual void draw(sf::RenderTarget& target, sf::RenderStates states) const;
+        virtual void drawCurrent(sf::RenderTarget& target, sf::RenderStates states) const;
+
         std::vector<Ptr> mChildren;
         SceneNode*  mParent;
 };
